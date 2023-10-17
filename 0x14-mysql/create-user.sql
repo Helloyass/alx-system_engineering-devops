af1ba0fb4812fd0c1cf00305abdcafa7eaa71de5
Helloyass <ghaayaaaa@gmail.com>
2023-10-17 12:28:42 -0700

    4-mysql_configuration_primary, 4-mysql_configuration_replica

--- 0x14-mysql/create-user.sql
+++
@@ -1,8 +0,0 @@
--- Create user to check DB status 
--- Run on the master server (task 1)
-
-CREATE USER IF NOT EXISTS 'Hello world'@'localhost'
-IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT  REPLICATION CLIENT, SELECT ON *.*
-TO 'Hello world'@'localhost';
-FLUSH PRIVILEGES;
