0c90c27ffbc1c4da77155a5b67be2b7eb882b609
Helloyass <ghaayaaaa@gmail.com>
2023-10-17 10:41:23 -0700

    server

--- 0x14-mysql/create-replica-user.sql
+++ 0x14-mysql/create-replica-user.sql
@@ -1,8 +1,8 @@
 -- Create user to check DB status 
--- Run on the master server (task 1)
+-- Run on the master server
 
-CREATE USER IF NOT EXISTS 'Hello world'@'localhost'
+CREATE USER IF NOT EXISTS 'replica_user'@'%'
 IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT  REPLICATION CLIENT, SELECT ON *.*
-TO 'Hello world'@'localhost';
+TO 'replica_user'@'%';
 FLUSH PRIVILEGES;
